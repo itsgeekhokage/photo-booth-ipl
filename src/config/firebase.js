fbe69243bc858430a9081fa090bb7044fb718d09
techkilla2016 <66670136+techkilla2016@users.noreply.github.com>
2024-03-28 14:24:02 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYlKu56uhmAGnNjAcVEFN2orwf7obHHUc",
-  authDomain: "ai-photo-grid-ipl.firebaseapp.com",
-  projectId: "ai-photo-grid-ipl",
-  storageBucket: "ai-photo-grid-ipl.appspot.com",
-  messagingSenderId: "68417247035",
-  appId: "1:68417247035:web:13a38ff11808fb28dc091e",
+  apiKey: "AIzaSyATchvbSvYyXHoQ_PNqTyHnAuZQUxs3dc8",
+  authDomain: "ai-video-test-28b14.firebaseapp.com",
+  projectId: "ai-video-test-28b14",
+  storageBucket: "ai-video-test-28b14.appspot.com",
+  messagingSenderId: "621998632535",
+  appId: "1:621998632535:web:2d30bf27864bd720963d12"
 };
 
 const app = initializeApp(firebaseConfig);
